2cf0bb2f5b567581fcef44bdd42afa1033a6d0a3
Andrew Gilbert <andy@agilbert.net>
2017-03-06 20:51:05 -0500

    added 2015 raw data

--- GetDataSets.R
+++ GetDataSets.R
@@ -1,22 +1,24 @@
 ##
 ## Support function to retrieve data sets
 ##
-
-dataset.2016_final_url <- "http://www.coolrunning.com/results/16/vt/Jul17_VT100E_set4.shtml"
-dataset.2016_final <- file.path("data", "2016_final.html")
-dataset.2016_splits_url <- "http://www.coolrunning.com/results/16/vt/Jul17_VT100E_set6.shtml"
-dataset.2016_splits <- file.path("data", "2016_splits.html")
+datasets <- list(c("2016_final.html", 
+                   "http://www.coolrunning.com/results/16/vt/Jul17_VT100E_set4.shtml"),
+                 c("2016_splits.html",
+                   "http://www.coolrunning.com/results/16/vt/Jul17_VT100E_set6.shtml"),
+                 c("2015_final.html",
+                   "http://www.coolrunning.com/results/15/vt/Jul19_VT100E_set1.shtml"),
+                 c("2015_splits.html",
+                   "http://www.coolrunning.com/results/15/vt/Jul19_VT100E_set2.shtml"))
 
 GetDataSets <- function() {
     ## Verifiy data dir exists and data files are present.
-    
     if (!file.exists("data")) {
         dir.create("data")
     }
-    if (!file.exists(dataset.2016_final)) {
-        download.file(dataset.2016_final_url, dataset.2016_final, method = "curl")
-    }
-    if (!file.exists(dataset.2016_splits)) {
-        download.file(dataset.2016_splits_url, dataset.2016_splits, method = "curl")
+    for (ds in datasets) {
+        fp <- file.path("data", ds[1])
+        if (!file.exists(fp)) {
+            download.file(ds[2], fp, method = "curl")
+        }
     }
 }
