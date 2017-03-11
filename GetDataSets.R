32eda3de33c860070ec26c7061c3735933a27737
Andrew Gilbert <andy@agilbert.net>
2017-03-11 10:00:43 -0500

    improving reading of splits, finals, aid stations

--- GetDataSets.R
+++ GetDataSets.R
@@ -10,10 +10,12 @@ datasets <- list(c("2015_final.html",
                  c("2016_splits.html",
                    "http://www.coolrunning.com/results/16/vt/Jul17_VT100E_set6.shtml"))
 
-# aid <- "https://docs.google.com/spreadsheets/d/19UDxQuK5WummNLvVgodsoPnSNRaVh2vBWr-f6lCYkDw/pubhtml"
+aid <- "https://docs.google.com/spreadsheets/d/19UDxQuK5WummNLvVgodsoPnSNRaVh2vBWr-f6lCYkDw/pubhtml"
 
 GetDataSets <- function() {
     ## Verifiy data dir exists and data files are present.
+    
+    # core results (splits and finals)
     if (!file.exists("data")) {
         dir.create("data")
     }
@@ -23,4 +25,11 @@ GetDataSets <- function() {
             download.file(ds[2], fp, method = "curl")
         }
     }
+    # aid station list
+    fp <- file.path("data", "aid_stations.html")
+    if (!file.exists(fp)) {
+        download.file(aid, fp, method = "curl")
+    }
 }
+
+
--- ReadRawData.R
+++ ReadRawData.R
@@ -1,14 +1,26 @@
 ##
 ## Read in raw data sets
 ##
-fp <- file.path("data", "2015_splits.html")
-splits_2015_raw <- htmltab(doc = fp, which = "//h3[text() = '100 Mile Splits']/following-sibling::table")
+library(htmltab)
+library(XML)
 
-fp <- file.path("data", "2016_splits.html")
-splits_2016_raw <- htmltab(doc = fp, which = "//h3[text() = '100M Splits']/following-sibling::table")
+ReadSplits <- function(year) {
+    fn <- paste(year, "_splits.html", sep="")
+    fp <- file.path("data", fn)
+    htmltab(doc = fp,
+            which = "//h3[contains(text(), 'Splits')]/following-sibling::table")
+    
+    #splits_2015_raw <- htmltab(doc = fp, 
+    #                           which = "//h3[text() = '100 Mile Splits']/following-sibling::table")
+    
+    #fp <- file.path("data", "2016_splits.html")
+    #splits_2016_raw <- htmltab(doc = fp, 
+    #                           which = "//h3[text() = '100M Splits']/following-sibling::table")
+}
 
-ReadFinalResults <- function(year) {
-    fp <- file.path("data", "2015_final.html")
+ReadFinals <- function(year) {
+    fn <- paste(year, "_final.html", sep="")
+    fp <- file.path("data", fn)
     raw_html <- htmlParse(fp)
     ns <- getNodeSet(raw_html, "//pre")
     xmlText <- xmlValue(ns[[1]])
@@ -17,4 +29,7 @@ ReadFinalResults <- function(year) {
     unlist(strsplit(xmlText, "\n"))
 }
 
-final_2015 <- ReadFinalResults()
+ReadAidStations <- function() {
+    aidfp <- file.path("data", "aid_stations.html")
+    readHTMLTable(aidfp, skip.rows = c(1:3, 30:40), stringsAsFactors = FALSE, which = 1)
+}
